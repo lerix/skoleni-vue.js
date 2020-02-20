4393c650922a174af920668771e28b0307f97eed
Tomáš Kalina <info@tomaskalina.cz>
2020-02-20 22:01:58 +0100

    Add laravel

---
+++ laravel/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                       {{message}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounteddddd.')
+        },
+        data() {
+            return { message: " I'm an example component!!!!!!!!!!!!"}
+        }
+    }
+</script>
---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('content')
+
+    <div id="app">
+     <example-component></example-component>
+     <example-component></example-component>
+     <example-component></example-component>
+    </div>
+
+    <script src="js/app.js"></script>
+@endsection
